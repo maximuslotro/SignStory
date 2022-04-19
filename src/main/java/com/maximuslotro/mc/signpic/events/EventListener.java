d0c9f35ca49acb7a976fb4c81507faf2b39ebd0d
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2022-04-19 15:30:31 -0500

    Fix for (to-do#13)

---
+++ src/main/java/com/maximuslotro/mc/signpic/events/EventListener.java
@@ -0,0 +1,29 @@
+package com.maximuslotro.mc.signpic.events;
+
+import com.maximuslotro.mc.signpic.Global_Vars;
+import com.maximuslotro.mc.signpic.Log;
+import com.maximuslotro.mc.signpic.entry.EntryId;
+import com.maximuslotro.mc.signpic.gui.GuiMain;
+import com.maximuslotro.mc.signpic.mode.CurrentMode;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent.ClientDisconnectionFromServerEvent;
+
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
+
+import java.util.List;
+
+public class EventListener {
+	@SubscribeEvent
+    public void onPlayerLeftServer(ClientDisconnectionFromServerEvent event) {
+		Global_Vars.CurrentPage = 0;
+		GuiMain.setContentId(EntryId.blank);
+		Global_Vars.Text.clear();
+		Global_Vars.Text = null;
+		CurrentMode.instance.setMode(CurrentMode.Mode.NONE);
+		CurrentMode.instance.setState(CurrentMode.State.PREVIEW, false);
+		Log.logDefault("Server Exited, Cleared Loaded Text");
+    }
+
+}
